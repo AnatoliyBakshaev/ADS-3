600a9a169c77cf79072338859aabfbaf4d8d35c2
AnatoliyBakshaev <nuklimzy@gmail.com>
2024-03-14 22:27:58 +0300

    ADS-3

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include "alg.h"
 
 int main() {
-    int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5};
-    int count = cbinsearch(arr, 15, 3); // count = 3
+    int size = 65536;
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = i;
+    int count = cbinsearch(arr, size, 1024);
+    delete arr;
+    std:: cout<< count;
     return 0;
 }
